ba29ec82e17184e45426e6d8e51baec1c6b32aba
robinjoshua <rjrobinjoshua@gmail.com>
2020-05-03 13:30:02 +0530

    Face regonition component development with clarifai api Retrieve imgae from given input that eill diplayed in FaceRegonition component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
+import Clarifai  from 'clarifai';
+import FaceRegonition from './components/FaceRegonition/FaceRegonition';
 
 const particleOptions = {
   particles: {
@@ -18,7 +20,40 @@ const particleOptions = {
   }
 }
 
+const app = new Clarifai.App({
+  apiKey: 'bd170755251e41b8b203d1e15a989e67'
+ });
+
 class App extends Component {
+
+
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+      imageUrl:''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value});
+  }
+  
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input});
+
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+      function(response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function(err) {
+        console.log(err);
+      }
+    );
+  }
+
 
   render() {
     return(
@@ -29,9 +64,8 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/* 
-        <FaceRegonition /> */}
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/> 
+        <FaceRegonition imageUrl={this.state.imageUrl}/>
       </div>
 
     )
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Tilt from 'react-tilt';
+import Tilt from 'react-parallax-tilt';
 import './Logo.css';
 import brain from './brain.png';
 
@@ -8,7 +8,11 @@ const Logo = () => {
 
     return(
             <div className='ma4 mt0'>   
-                <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+                <Tilt className="Tilt br2 shadow-2" 
+                tiltReverse={true}
+                tiltMaxAngleX={35}
+                tiltMaxAngleY={35}
+                style = {{ height: 150, width: 150 }}>
                     <div className="Tilt-inner pa3">
                         <img style={{paddingTop: '5px'}} alt="logo" src={brain} />
                     </div>
