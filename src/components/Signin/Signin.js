529526a6ce1a5e7262efacbc35cc4a66a70a405a
robinjoshua <rjrobinjoshua@gmail.com>
2020-05-09 16:37:02 +0530

    added validation to sign in form

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,24 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 
+const signinSchema = yup.object().shape({
+    email: yup.string().email("Email must be a valid email").required("Email is required"),
+    password: yup.string()
+    .required("No password provided")
+});
 
 const Signin = (props) => {
 
     const smartBrainApiUrl = process.env.REACT_APP_SMART_BRAIN_API_URL;
 
-    const signinSchema = yup.object().shape({
-        email: yup.string().email().required("Required"),
-        password: yup.string()
-        .required("No password provided.")
-    });
+    const logInCard = useRef(null);
 
-    const { register, handleSubmit, errors } = useForm({
+    const [signInEmail, setSignInEmail] = useState('');
+    const [signInPassword, setSignInPassword] = useState('');
+
+
+    const { register, handleSubmit, errors, setError } = useForm({
         validationSchema: signinSchema,
         mode: "onBlur"
     });
-    const [signInEmail, setSignInEmail] = useState('');
-    const [signInPassword, setSignInPassword] = useState('');
+
 
     const onEmailChange =(event) => {
         setSignInEmail(event.target.value);
@@ -42,28 +46,49 @@ const Signin = (props) => {
             if(data.id){
                 props.loadUser(data);
                 props.onRouteChange('home');
+            }else{
+                setError("password","notMatch","Invalid credentials");
+                console.log(errors);
+                shakeLoginCard();
             }
         })
     }
 
+    const onSubmitKeyPress = (event) => {
+        console.log('event', event);
+        var key=event.keyCode || event.which;
+        if (key === 13){
+            onSubmitSignIn();
+  }
+    }
+
+    const shakeLoginCard = () => {
+
+        logInCard.current.classList.add('error-shake');
+        setTimeout(() => {
+            logInCard.current.classList.remove('error-shake');
+        }, 300);
+    }
+
     const { onRouteChange } = props;
     return(
-        <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+        <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center" ref= {logInCard}>
             <main className="pa4 black-80">
                 <div className="measure">
-                    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                        <legend className="f2 fw6 ph0 mh0">Sign In</legend>
+                    <fieldset id="sign_in" className="ba b--transparent ph0 mh0">
+                        <legend className="f2 fw6 ph0 mh0">Sign In</legend> 
                         <div className="mt3">
                         <label className="db fw6 lh-copy f6" htmlFor ="email-address">Email</label>
                         <input onChange={onEmailChange} className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
                             type="email" name="email"  id="email-address" ref={register} />
-                        {errors.email && <p>{errors.email.message}</p>}
+                        {errors.email && <p className="error">{errors.email.message}</p>}
                         </div>
                         <div className="mv3">
                         <label className="db fw6 lh-copy f6" htmlFor ="password">Password</label>
-                        <input onChange={onPasswordChange} className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                        <input onChange={onPasswordChange} onKeyPress={event => handleSubmit(onSubmitKeyPress(event))} className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
                             type="password" name="password"  id="password" ref={register} />
-                        {errors.password && <p>{errors.password.message}</p>}    
+                        {console.log('errors', errors)}
+                        {errors.password && <p className="error">{errors.password.message}</p>}    
                         </div>
                     </fieldset>
                     <div className="">
@@ -74,7 +99,7 @@ const Signin = (props) => {
                         />
                     </div>
                     <div className="lh-copy mt3">
-                        <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
+                        <p onClick={() => onRouteChange('register')} className="f6 b link dim black db pointer">Register</p>
                     </div>
                 </div>
             </main>
